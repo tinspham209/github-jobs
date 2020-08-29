42f3f5e26e63c8270231ce1ae432905861a4996a
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-29 23:49:51 +0700

    Fetch API and implement to Job component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { useFetchJobs } from "./hooks/useFetchJob";
+import { Container } from "react-bootstrap";
+import Job from "./Job";
 const App = () => {
+	const [params, setParams] = useState({});
+	const [page, setPage] = useState(1);
+	const { jobs, loading, error } = useFetchJobs(params, page);
 	return (
-		<div>
-			<h2>Hello world</h2>
-		</div>
+		<Container>
+			{loading && <h1>Loading...</h1>}
+			{error && <h1>Error. Try Refreshing.</h1>}
+			{jobs.map((job) => {
+				return <Job key={job.id} job={job} />;
+			})}
+		</Container>
 	);
 };
 
