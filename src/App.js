9c3ebb55409bc9e21083a5a3e9ebbe6cb79d48c9
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-30 01:20:24 +0700

    handleParamChange fucntion

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ import { Container } from "react-bootstrap";
 import Job from "./components/Job";
 import Pagination from "./components/Pagination";
 import Spinner from "./components/UI/Spinner";
+import SearchForm from "./components/SearchForm";
 const App = () => {
 	const [params, setParams] = useState({});
 	const [page, setPage] = useState(1);
 	const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
+
+	const handleParamChange = (e) => {
+		const param = e.target.name;
+		const value = e.target.value;
+		setPage(1);
+		setParams((prevParams) => {
+			return { ...prevParams, [param]: value };
+		});
+	};
+
 	return (
 		<Container className="my-4">
 			<h1 className="mb-4">Github Jobs</h1>
+			<SearchForm params={params} onParamChange={handleParamChange} />
 			<Pagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
 			{loading && <Spinner />}
 			{error && <h1>Error. Try Refreshing.</h1>}
