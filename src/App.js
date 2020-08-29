0947f503d8301505488a5d5b9188e92eea8c796f
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-30 01:03:36 +0700

    update Folder structure

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import "./App.css";
 import { useFetchJobs } from "./hooks/useFetchJob";
 import { Container } from "react-bootstrap";
-import Job from "./Job";
-import JobsPagination from "./JobsPagination";
+import Job from "./components/Job";
+import Pagination from "./components/Pagination";
 import Spinner from "./components/UI/Spinner";
 const App = () => {
 	const [params, setParams] = useState({});
@@ -12,13 +12,13 @@ const App = () => {
 	return (
 		<Container className="my-4">
 			<h1 className="mb-4">Github Jobs</h1>
-			<JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+			<Pagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
 			{loading && <Spinner />}
 			{error && <h1>Error. Try Refreshing.</h1>}
 			{jobs.map((job) => {
 				return <Job key={job.id} job={job} />;
 			})}
-			<JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+			<Pagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
 		</Container>
 	);
 };
---
+++ src/components/SearchForm/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchForm = ({ params, onParamChange }) => {
+	return (
+		<div>
+			<h2>Search Form</h2>
+		</div>
+	);
+};
+
+export default SearchForm;
