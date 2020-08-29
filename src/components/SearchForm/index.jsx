5359d65e0272dc0aac684e39482c03785b58dbdf
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-30 01:20:06 +0700

    fix warning uncontrolled component at value

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -9,7 +9,7 @@ const SearchForm = ({ params, onParamChange }) => {
 					<Form.Label>Description</Form.Label>
 					<Form.Control
 						onChange={onParamChange}
-						value={params.description}
+						value={params.description || ""}
 						name="description"
 						type="text"
 					/>
@@ -18,7 +18,7 @@ const SearchForm = ({ params, onParamChange }) => {
 					<Form.Label>Location</Form.Label>
 					<Form.Control
 						onChange={onParamChange}
-						value={params.location}
+						value={params.location || ""}
 						name="location"
 						type="text"
 					/>
@@ -26,7 +26,7 @@ const SearchForm = ({ params, onParamChange }) => {
 				<Form.Group as={Col} xs="auto" className="ml-2">
 					<Form.Check
 						onChange={onParamChange}
-						value={params.full_time}
+						value={params.full_time || ""}
 						name="full_time"
 						id="full-time"
 						label="Only Full Time"
